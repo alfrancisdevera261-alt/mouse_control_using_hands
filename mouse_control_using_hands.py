9512336dde722931a950162b34cb2ef981c7d12c
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-03 23:53:08 +0800

    Adjusted the distance threshold value and remove prints

--- mouse_control_using_hands.py
+++ mouse_control_using_hands.py
@@ -20,23 +20,21 @@ while True:
             for id, lm in enumerate(one_hand_landmarks):
                 x = int(lm.x * image_width)
                 y = int(lm.y * image_height)
-                print(x,    y)
                 if id == 8:
                     mouse_x = int(screen_width / image_width * x)
                     mouse_y = int(screen_height / image_height * y)
                     pyautogui.moveTo(mouse_x, mouse_y)
-                    cv2.circle(image,(x,y),3,(0,255,255),cv2.FILLED)
+                    cv2.circle(image,(x,y),3,(0,255,255), cv2.FILLED)
                     x1 = x
                     y1 = y
                 if id == 4:
                     x2 = x
                     y2 = y
-                    cv2.circle(image,(x,y),3,(0,255,255),cv2.FILLED)
+                    cv2.circle(image,(x,y),3,(0,255,255), cv2.FILLED)
         distance = y2 - y1
-        print(distance)
-        if distance < 20:
+        if distance < 40:
             pyautogui.click()
-                
+            print("Click")   
     cv2.imshow("Hand movement video capture", image)
     key = cv2.waitKey(100)
     if key == 27:

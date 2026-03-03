15710e9af31ee7e2149961844e189ec805df8684
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-03 22:54:42 +0800

    Code Finished

--- mouse_control_using_hands.py
+++ mouse_control_using_hands.py
@@ -3,9 +3,12 @@ import mediapipe
 import pyautogui
 capture_hands = mediapipe.solutions.hands.Hands()
 drawing_option = mediapipe.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
 camera = cv2.VideoCapture(0)
+x1 = y1 = x2 = y2 = 0
 while True:
     _,image = camera.read()
+    image_height, image_width, _ = image.shape
     image = cv2.flip(image,1)
     rgb_iamge = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     output_hands = capture_hands.process(rgb_iamge)
@@ -13,6 +16,27 @@ while True:
     if all_hands:
         for hand in all_hands:
             drawing_option.draw_landmarks(image,hand)
+            one_hand_landmarks = hand.landmark
+            for id, lm in enumerate(one_hand_landmarks):
+                x = int(lm.x * image_width)
+                y = int(lm.y * image_height)
+                print(x,    y)
+                if id == 8:
+                    mouse_x = int(screen_width / image_width * x)
+                    mouse_y = int(screen_height / image_height * y)
+                    pyautogui.moveTo(mouse_x, mouse_y)
+                    cv2.circle(image,(x,y),3,(0,255,255),cv2.FILLED)
+                    x1 = x
+                    y1 = y
+                if id == 4:
+                    x2 = x
+                    y2 = y
+                    cv2.circle(image,(x,y),3,(0,255,255),cv2.FILLED)
+        distance = y2 - y1
+        print(distance)
+        if distance < 20:
+            pyautogui.click()
+                
     cv2.imshow("Hand movement video capture", image)
     key = cv2.waitKey(100)
     if key == 27:

c1ccca0a5710819acad4ff98c1b0ecdf9d6ca706
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-03-04 21:02:13 +0800

    Fixed variable name spelling

--- mouse_control_using_hands.py
+++ mouse_control_using_hands.py
@@ -10,8 +10,8 @@ while True:
     _,image = camera.read()
     image_height, image_width, _ = image.shape
     image = cv2.flip(image,1)
-    rgb_iamge = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    output_hands = capture_hands.process(rgb_iamge)
+    rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    output_hands = capture_hands.process(rgb_image)
     all_hands = output_hands.multi_hand_landmarks
     if all_hands:
         for hand in all_hands:
